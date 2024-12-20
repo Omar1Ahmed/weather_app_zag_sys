f589f26fae7fdc241dea8ec84199d147ae9e5547
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2024-12-20 02:46:45 +0200

    add color to card

--- lib/theming/colors.dart
+++ lib/theming/colors.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
 class ColorsManager {
+
+   static const Color cardBackgrounColor = Color.fromARGB(255, 18, 2, 193);
   static const Color primaryColor = Color(0xFF1F1338);
   static const Color secondaryColor = Color(0xFF2A2A2E);
   static const Color buttonColor = Color(0xFF7e64ff);
